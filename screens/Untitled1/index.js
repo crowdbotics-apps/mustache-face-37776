423e59cb5d90133e40a22ec0c6d73d1e88397bfa
155928@epsstudents.org <155928@epsstudents.org>
2022-12-02 13:33:49 -0500

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.JZNpRTRe}>
+      </View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  JZNpRTRe: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%",
+    transform: "rotate(120deg)",
+    top: -637,
+    left: 0
+  }
+});
